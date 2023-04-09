ae9a181e8eb5aa6dffd5be256893facc642b0633
Egoist <epsilonelittr@gmail.com>
2023-04-09 19:25:54 +0300

    Add sluggable trait

---
+++ src/Traits/Sluggable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Builder;
+
+trait Sluggable
+{
+    public function bootSluggable()
+    {
+        static::creating(function ($model) {
+            $model->slug = $model->slug ?: Str::slug($model->title);
+        });
+    }
+
+    public function scopeSlug(mixed $query, string $slug): Builder
+    {
+        return $query->where('slug', $slug);
+    }
+
+    public function scopeSlugLike(mixed $query, string $slug): Builder
+    {
+        return $query->where('slug', 'like', "%{$slug}%");
+    }
+
+    public function scopeSlugNot(mixed $query, string $slug): Builder
+    {
+        return $query->where('slug', '!=', $slug);
+    }
+
+    public function scopeSlugNotLike(mixed $query, string $slug): Builder
+    {
+        return $query->where('slug', 'not like', "%{$slug}%");
+    }
+
+    public function scopeSlugIn(mixed $query, array $slugs): Builder
+    {
+        return $query->whereIn('slug', $slugs);
+    }
+
+    public function scopeSlugNotIn(mixed $query, array $slugs): Builder
+    {
+        return $query->whereNotIn('slug', $slugs);
+    }
+}
