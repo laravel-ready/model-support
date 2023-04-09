e644e27ac77b127c6d594d70293dd88390b1141b
Egoist <epsilonelittr@gmail.com>
2023-04-09 20:04:56 +0300

    Add type cast

--- src/Traits/HasActive.php
+++ src/Traits/HasActive.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait HasActive
 {
+    public function bootHasActive(): void
+    {
+        // append the is_active column to the fillable array
+        $this->fillable[] = 'is_active';
+
+        // add the is_active column to the casts array
+        $this->casts['is_active'] = 'boolean';
+    }
+
     public function scopeStatus(mixed $query, bool $status): Builder
     {
         return $query->where('is_active', $status);
