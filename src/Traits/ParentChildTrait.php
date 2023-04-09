884edca9eb8e10bde28a410b21ee60753b232463
Egoist <epsilonelittr@gmail.com>
2023-04-09 19:28:24 +0300

    Add parent child trait

---
+++ src/Traits/ParentChildTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+trait ParentChild
+{
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+
+    public function recursiveParent(): mixed
+    {
+        return $this->parent()->with('recursiveParent');
+    }
+
+    public function recursiveChildren(): mixed
+    {
+        return $this->children()->with('recursiveChildren');
+    }
+
+    public function recursiveParentAndChildren(): mixed
+    {
+        return $this->recursiveParent()->with('recursiveChildren');
+    }
+}
