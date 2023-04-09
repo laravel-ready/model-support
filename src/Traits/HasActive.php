50173cfc533b00fad751609c31d0ed88dd531fe0
Egoist <epsilonelittr@gmail.com>
2023-04-09 19:27:51 +0300

    Add has active trait

---
+++ src/Traits/HasActive.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+
+
+trait HasActive
+{
+    public function scopeStatus(mixed $query, bool $status): Builder
+    {
+        return $query->where('is_active', $status);
+    }
+
+    public function scopeActive(mixed $query): Builder
+    {
+        return $query->where('is_active', true);
+    }
+
+    public function scopeInactive(mixed $query): Builder
+    {
+        return $query->where('is_active', false);
+    }
+}
