048d181187fbbbf12fa4b6d50712c2b867da3c44
relliv <epsilonelittr@gmail.com>
2023-09-16 20:23:25 +0300

    Fix wrong initialize method names

--- src/Traits/SluggableName.php
+++ src/Traits/SluggableName.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait SluggableName
 {
-    public function initializeSluggable(): void
+    public function initializeSluggableName(): void
     {
         static::creating(function ($model) {
             $model->slug = Str::slug($model->name);
--- src/Traits/SluggableTitle.php
+++ src/Traits/SluggableTitle.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait SluggableTitle
 {
-    public function initializeSluggable(): void
+    public function initializeSluggableTitle(): void
     {
         static::creating(function ($model) {
             $model->slug = Str::slug($model->title);
